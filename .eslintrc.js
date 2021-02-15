f34d901f1afbda619e2d817248e748f2a5566fc6
Taylor Everding <taylor1791@users.noreply.github.com>
2021-02-14 19:04:22 -0700

    Lint JavaScript in the CI Suite

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    sourceType: "module",
+  },
+  rules: {},
+};
